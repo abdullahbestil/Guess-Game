41729867de545e4cff1b05c1f1278281bf0e362b
unknown <abestil14@gmail.com>
2018-09-24 14:23:01 +0300

    Guess Game 1.0 edition

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Abdullah Bestil
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        GuessGame form;
+        form = new GuessGame();
+        form.setVisible(true);
+    }
+    
+}
